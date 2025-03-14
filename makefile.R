fde15bd7f10ca6581cff064138d6812422e483cc
embruna <embruna@ufl.edu>
2025-03-14 09:45:19 -0400

    adding session info and package cpature

--- makefile.R
+++ makefile.R
@@ -1,8 +1,18 @@
-
 #### Template of a master script for the project ####
 
 ## Using an advanced tool like {drake} or {targets} is recommended,
 ## but this can work as a simple alternative.
+
+
+# Packages Required -------------------------------------------------------
+
+# install.packages("grateful")
+# install.packages("here")
+# install.packages("tidyverse")
+library(grateful)
+library(here)
+library(tidyverse)
+
 
 
 ### if you have written specific functions in R/, load the package:
@@ -11,36 +21,26 @@
 
 ### Source data cleaning scripts
 # source("data-raw/datacleaning.R")
+
 ### Could also be Rmarkdown:
 # rmarkdown::render("data-raw/datacleaning.Rmd")
 
 
-### Render manuscript
-# rmarkdown::render("manuscript/ms_project.Rmd")
-
 
 ## Check your code ##
+
 # goodpractice::gp()
 
 
 
-#### Control package dependencies ####
 
-# Many alternatives available, for example:
+# Generate Session Info and list of packages used -------------------------
 
-# sessionInfo()
-
-# automagic::make_deps_file()
-
-# Or using renv:
-# renv::init()
-# renv::snapshot()
-
-# Or use Docker, see e.g. {containerit} or {liftr}
+source(here("code", "packages_and_session_info.R"))
+packages_and_session_info()
 
 
 ## Make a website for your project?
 ## see https://pkgdown.r-lib.org/
 # usethis::use_pkgdown()
 # pkgdown::build_site()
-
